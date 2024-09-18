56f076134c32c306e29fd295d24ba66ee3775275
Duong Intel <duongintel@Duongs-MacBook-Pro.local>
2024-09-18 15:47:55 -0600

    move db info out

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ app.use(express.json());
 const pool = new Pool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
-  database: 'postgres',
-  user: 'postgres',
-  password: 'admin'
+  database: process.env.DB_DATABASE,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD
 });
 
 // Function to initialize the database
